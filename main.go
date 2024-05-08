b8a9e8e4460014c7f2297e8bd21c96507474d442
Andrew Pham <andrew.pham@shopify.com>
2024-05-08 14:00:28 -0400

    Add entry point

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/rivo/tview"
+	"time"
+)
+
+func getCurrentFormattedDate() string {
+	return time.Now().Format("01/02/2006")
+}
+
+func main() {
+    app := tview.NewApplication()
+    state := &AppState{
+			Date: getCurrentFormattedDate(),
+		}
+    setupAppUI(app, state)
+    if err := app.Run(); err != nil {
+        panic(err)
+    }
+}
